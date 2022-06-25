735b87f115caf6e3f9dda44250ef0bca4393ee57
Ankur Prakash <ankur.prakash@mavenir.com>
2022-06-26 05:05:46 +0530

    Understanding Future

--- Combine.playground/Contents.swift
+++ Combine.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
+import Combine
 
-var greeting = "Hello, playground"
+var store = Set<AnyCancellable>()
+
+var currentValueSubject = CurrentValueSubject<Bool, Never>(true)
+let future = Future<Bool, Error> { promise in
+    promise(.success(true))
+}
+
+print("Hello")
+future
+    .sink { completion in
+        print("Future completion")
+
+    } receiveValue: { value in
+        print("Future value = \(value)")
+    }
+    .store(in: &store)
+
+
+currentValueSubject
+    .first()
+    .flatMap { _ in
+        Future<Bool, Error> { promise in
+            promise(.success(false))
+        }
+    }.sink { completion in
+        print("Future completion")
+
+    } receiveValue: { value in
+        print("Future value = \(value)")
+    }.store(in: &store)
+
